899f2147dd4a38daa2a0533252c68c8525a44314
ArtemAlt <artem.altunin.vit@gmail.com>
2021-05-07 00:11:47 +0300

    Initial commit

---
+++ src/main/java/ITBlogPublisher.java
@@ -0,0 +1,41 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+public class ITBlogPublisher {
+    private static final String EXCHANGE_NAME = "NEWS-CHANNEL";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("123456");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            System.out.println("Welcome to command line 'IT BLOG ' ");
+            System.out.println("Enter your news. Example - php * some message");
+            Scanner in = new Scanner(System.in);
+            String topic;
+            do {
+                String inputCommand = in.nextLine();
+                String[] separateInput = inputCommand.split(" ");
+                topic = separateInput[0];
+                StringBuilder outMessage= new StringBuilder();
+
+                for (int i = 2; i < separateInput.length; i++) {
+                  outMessage.append(separateInput[i]).append(" ");
+                }
+                channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+                channel.basicPublish(EXCHANGE_NAME, topic, null, outMessage.toString().getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Sent message channel - " + topic + " message - " + outMessage);
+            } while (!topic.equals("exit"));
+
+
+        }
+    }
+}
---
+++ src/main/java/ITConsumer.java
@@ -0,0 +1,44 @@
+import com.rabbitmq.client.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ITConsumer {
+    private static final String EXCHANGE_NAME = "NEWS-CHANNEL";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("123456");
+
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            String queueName = channel.queueDeclare().getQueue();
+            System.out.println("My queue name: " + queueName);
+            System.out.println("Welcome to command line 'IT BLOG NEWS ' ");
+            System.out.println("Enter your subscription. Example - set_topic php");
+            Scanner in = new Scanner(System.in);
+            String topic;
+            do {
+                String inputCommand = in.nextLine();
+                String[] separateInput = inputCommand.split(" ");
+                topic = separateInput[1];
+                channel.queueBind(queueName, EXCHANGE_NAME, topic);
+                System.out.println(" [*] Waiting for messages from channel - "+topic);
+                DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    System.out.println(" [x] Received '" + message + "'");
+                };
+                channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+                });
+
+
+            } while (!topic.equals("exit"));
+
+        }
+    }
+}
